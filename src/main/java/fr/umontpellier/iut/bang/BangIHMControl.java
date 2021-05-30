b48724000693d900d597090de9294379cefee672
BastienGavioli <bastien.gavioli34@gmail.com>
2021-05-30 15:58:33 +0200

    Ajout d'un bouton pour quitter

--- src/main/java/fr/umontpellier/iut/bang/BangIHMControl.java
+++ src/main/java/fr/umontpellier/iut/bang/BangIHMControl.java
@@ -1,11 +1,15 @@
 package fr.umontpellier.iut.bang;
 
 import fr.umontpellier.iut.bang.views.GameView;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class BangIHMControl extends GameView {
 
@@ -20,6 +24,9 @@ public class BangIHMControl extends GameView {
 
     @FXML
     private Button parametres;
+
+    @FXML
+    private Button quitter;
 
     public BangIHMControl(IGame game) {
         super(game);
@@ -42,5 +49,16 @@ public class BangIHMControl extends GameView {
     @Override
     protected void setPassSelectedListener() {
 
+    }
+
+    @FXML
+    public void quitGame() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Nigerundayo, Smokey!");
+        alert.setContentText("Do you really want to stop playing ?");
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            Platform.exit();
+        }
     }
 }
