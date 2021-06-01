b8481af836f6bb86db802485a5d0abda40adb3ca
Gatien Depeyre <gatien.depeyre@etu.umontpellier.fr>
2021-06-01 19:02:46 +0200

    ingameVue implementé un peu

--- src/main/java/fr/umontpellier/iut/bang/views/ourviews/InGameView.java
+++ src/main/java/fr/umontpellier/iut/bang/views/ourviews/InGameView.java
@@ -1,13 +1,30 @@
 package fr.umontpellier.iut.bang.views.ourviews;
 
+import fr.umontpellier.iut.bang.ICard;
 import fr.umontpellier.iut.bang.IGame;
+import fr.umontpellier.iut.bang.logic.cards.CardSuit;
+import fr.umontpellier.iut.bang.logic.cards.CatBalou;
 import fr.umontpellier.iut.bang.views.GameView;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 
 import java.io.IOException;
 
 public class InGameView extends GameView {
+
+    @FXML
+    private Button testCard;
+
+    @FXML
+    private Button retourMenuBtn;
+
+    @FXML
+    private Button passBtn;
+
+    @FXML
+    private Button parametresBtn;
 
     public InGameView(IGame game) {
         super(game);
@@ -22,6 +39,13 @@ public class InGameView extends GameView {
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
+
+        placementInitial();
+    }
+
+    public void placementInitial(){
+        ICard carteTest = new ICard(new CatBalou(1, CardSuit.CLUB), 50, 50);
+        testCard.setGraphic(new ImageView("src/main/resources/"+carteTest.getImageName()));
 
     }
 
